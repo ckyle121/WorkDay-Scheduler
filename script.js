f83800ced3e901c2e7a15ff4a5d9da380b68fb59
ckyle121 <carolinekyle21@gmail.com>
2022-01-11 10:20:12 -0600

    display time

---
+++ script.js
@@ -0,0 +1,9 @@
+// Display Current Date & Time with Moment.js
+currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").text(currentDay);
+
+// color code time blocks to indicate whether time is in the past, present, or future
+
+// Enter tasks and save to local storage
+
+// tasks appear from local storage after refresh 
