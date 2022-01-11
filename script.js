003c9792436798ec5c06d0a8cd2dc2ffd53164b8
ckyle121 <carolinekyle21@gmail.com>
2022-01-11 12:17:29 -0600

    update html classes for css styling

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(currentDay);
 
 // color code time blocks to indicate whether time is in the past, present, or future
+function colorTime(){
+    // if color is in past add class past
+    // if color is current add class present and class hour
+    // if color is future time add class future 
+}
+
 
 // Enter tasks and save to local storage
+localStorage.setItem("tasks", JSON.stringify());
+
 
 // tasks appear from local storage after refresh 
+localStorage.getItem("tasks")
